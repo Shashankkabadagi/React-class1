568d04650f71adc62bf5718a9e13a0023caa1684
ShashankKabadagi <Shashankbk485@gmil.com>
2024-12-20 19:51:41 +0530

    delete function

--- Reactpractice/src/App.jsx
+++ Reactpractice/src/App.jsx
@@ -2,42 +2,56 @@ import { useState } from "react";
 import { Card } from "./component/Card";
 import Homepage from "./component/Homepage";
 
+
+const users=[
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer'
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer'
+  }
+]
+
  function App(){
 
-   const[searchInput,setsearchInput]=useState('')
 
-  const users=[
-    {
-      uniqueNo: 1,
-      imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-      name: 'Esther Howard',
-      role: 'Software Developer'
-    },
-    {
-      uniqueNo: 2,
-      imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
-      name: 'Floyd Miles',
-      role: 'Software Developer'
-    },
-    {
-      uniqueNo: 3,
-      imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
-      name: 'Jacob Jones',
-      role: 'Software Developer'
-    },
-    {
-      uniqueNo: 4,
-      imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
-      name: 'Devon Lane',
-      role: 'Software Developer'
-    }
-  ]
+   const[searchInput,setsearchInput]=useState('')
+   const[dynamicUsers,setdynamicUsers]=useState(users)
+
+
+
+  const  deleteFu= (id)=>{
+    const list= dynamicUsers.filter((eachitem)=>{
+    return(
+        eachitem.uniqueNo!==id
+    )
+  })
+  setdynamicUsers(list)
+  }
 
   function inputChange(event){
     setsearchInput(event.target.value)
   }
 
-  const filteredUser=users.filter((eachitem)=>{
+  const filteredUser=dynamicUsers.filter((eachitem)=>{
     return(
       eachitem.name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())
     )
@@ -50,9 +64,9 @@ import Homepage from "./component/Homepage";
         <input type="text" placeholder="search" onChange={inputChange} className="w-80 border-4 mx-8 border-gray-500 rounded-lg"/>
         
           {
-            filteredUser.map((eachitem)=>{
+            filteredUser.map((eachitem,index)=>{
               return (
-              <Card imageUrl={eachitem.imageUrl} name={eachitem.name} Role={eachitem.role}></Card>
+              <Card key={index} imageUrl={eachitem.imageUrl} id={eachitem.uniqueNo} deleteFU={deleteFu} name={eachitem.name} Role={eachitem.role}></Card>
               )
             })
           }
--- Reactpractice/src/component/Card.jsx
+++ Reactpractice/src/component/Card.jsx
@@ -1,5 +1,11 @@
 export function Card(props){
-    const{imageUrl,name,Role}=props
+    const{imageUrl,name,Role,id,deleteFU}=props
+
+    function deleteIt(){
+        deleteFU(id)
+        console.log(id)
+    }
+
     return (
         <div className="flex justify-center w-80 border-4 mx-8 m-2 border-gray-500 rounded-xl">
             <div className="my-2">
@@ -8,7 +14,7 @@ export function Card(props){
             <div className="pt-3">
                 <h3 className="font-semibold" >name:<span className="font-bold">{name}</span></h3>
                 <p className="font-semibold">Role:<span className="font-bold text-blue-400">{Role}</span></p>
-                <button className="border-2 bg-blue-300 border-red-950 rounded-md font-semibold">Delete</button>
+                <button className="border-2 bg-blue-300 border-red-950 rounded-md font-semibold" onClick={deleteIt}>Delete</button>
             </div>
         </div> 
     )
